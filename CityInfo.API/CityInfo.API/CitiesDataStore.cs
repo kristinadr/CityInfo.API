bf65c1bf2e8350f5881dda004ff9a0c26066aaf3
kristinadr <36666564+kristinadr@users.noreply.github.com>
2018-03-05 09:32:02 +0200

    Created API project with GET metods
    GET cities
    GET city by ID

---
+++ CityInfo.API/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with the big park."
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Kaunas",
+                    Description = "The one where two rivers meet."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower."
+                }
+            };
+        }
+    }
+}
